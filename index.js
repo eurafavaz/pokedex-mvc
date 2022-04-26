00587bf8bbc9a8ed2eb02717e6dfbe539c1fdd13
Rafael Vaz <eurafavaz@gmail.com>
2022-04-26 17:23:14 -0300

    version_render_final

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
